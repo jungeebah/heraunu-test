2f056820acd29165c5a95e3d9613e0bf04ac0e95
jungee <jungebah@gmail.com>
2020-08-29 16:53:31 -0700

    added app bar

---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from "enzyme-adapter-react-16";
+import Header from './Header'
+
+Enzyme.configure({ adapter: new Adapter() });
+describe("Header", () => {
+    it("should render Header", () => {
+        const wrapper = shallow(<Header />)
+    })
+})
