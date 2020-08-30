e0b479445d4dec6832901ffdcecc2d98c9710606
jungee <jungebah@gmail.com>
2020-08-29 17:01:22 -0700

    updated app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from "enzyme-adapter-react-16";
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapter() });
+describe("App", () => {
+  it("should render App", () => {
+    const wrapper = shallow(<App />)
+  })
+})
